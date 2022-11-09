e4e2a7a2d2833ae542a6a72d9db87bc3f22dcf92
Jack <jackfox@tuta.io>
2022-11-09 11:09:04 +0300

    bananduck

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useRef, useState, Suspense } from "react";
+import * as THREE from "three";
+import { useGLTF, Environment } from "@react-three/drei";
+
+import Banana from "./components/Banana";
+import { EffectComposer, DepthOfField } from "@react-three/postprocessing";
+
+type AppProps = {
+  count: number;
+  depth: number;
+};
+
+export default function App({ count = 150, depth = 100 }: AppProps) {
+  return (
+    <Canvas gl={{ alpha: false }} camera={{ near: 0.01, far: 110, fov: 30 }}>
+      <color attach="background" args={["#ffe07c"]} />
+      {/* <ambientLight intensity={0.2} /> */}
+      {/* <spotLight position={[10, 10, 10]} intensity={1} /> */}
+      <Suspense fallback={null}>
+        <Environment preset="sunset" />
+        {Array.from({ length: count }, (_, i) => (
+          <Banana key={i} z={-(i / count) * depth -10} />
+        ))}
+        <EffectComposer>
+          <DepthOfField
+            target={[0, 0, depth / 5]}
+            focalLength={0.5}
+            bokehScale={11}
+            height={800}
+          />
+        </EffectComposer>
+      </Suspense>
+    </Canvas>
+  );
+}
